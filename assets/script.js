fa27e3b43e8ae69963e8352d814d3d7c3304aad7
The-Schwiz <113313870+The-Schwiz@users.noreply.github.com>
2022-11-12 13:29:33 -0500

    update JS with logic

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,126 @@
-const currentDayEl = $('#currentDay');
-const currentTime = dayjs().startOf('hour');
 
-const nineAmEl = $('#hour-9');
-const tenAmEl = $('#hour-10');
-const elevenAmEl = $('#hour-11');
-const twelvePmEl = $('#hour-12');
-const onePmEl = $('#hour-13');
-const twoPmEl = $('#hour-14');
-const threePmEl = $('#hour-15');
-const fourPmEl = $('#hour-16');
-const fivePmEl = $('#hour-17');
-
-// Do we need this?:
-// const pastEl = isBefore(currentTime);
-// const presentEl = currentTime;
-// const futureEl = isAfter(currentTime);
-
-//Displays date and time in header
-function displayDateAndTime() {
-  const rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  currentDayEl.text(rightNow);
+let hourTexts = {
+    '9': '',
+    '10':'',
+    '11': '',
+    '12': '',
+    '13': '',
+    '14': '',
+    '15': '',
+    '16': '',
+    '17': '',
 }
 
-// Working through adding style based on if time is past present or future
-function compareToCurrentTime () {
-const currentTime = dayjs().startOf('hour');
+// Displays current date and time
+let rightNow = dayjs();
+let before = rightNow;
 
-// Should I set each of these to store a 24 hr time value?
-const nineAmEl = "09:00:00"
-const tenAmEl = "10:00:00"
-const elevenAmEl = "11:00:00";
-const twelvePmEl = "12:00:00";
-const onePmEl = "13:00:00";
-const twoPmEl = "14:00:00";
-const threePmEl = "15:00:00";
-const fourPmEl = "16:00:00";
-const fivePmEl = "17:00:00";
+//executes Jquery only once entire page loads
+$(document).ready(function () {
+    $('#currentDay').html(rightNow);
+    setInterval(function(){
+        $('#currentDay').html(rightNow.format("MMM DD YYYY [at] hh:mm:ss a"));
+        // when hour changes, update the styles/classes of the present and next future timeblocks
+        if (rightNow.hour() > before.hour()) {
+            // edge case: make sure before hour is valid between 9am to 5pm
+            if (before.hour() >= 9 && before.hour() <= 17) {
+                const timeBlockTextBefore = $(`#hour${before.hour()} .plans`).eq(0);
+                timeBlockTextBefore.removeClass("present");
+                timeBlockTextBefore.addClass("past");
+            }
+            // edge case: make sure rightnow hour is valid between 9am to 5pm
+            if (rightNow.hour() >= 9 && rightNow.hour() <= 17) {
+                const timeBlockTextNow = $(`#hour${rightNow.hour()} .plans`).eq(0);
+                timeBlockTextNow.removeClass("future");
+                timeBlockTextNow.addClass("present");
+            }
+        }
 
-// Or should I store these in array with key/value pair?
-
-}
- 
-//Example of comparing times:
-// if (projectDate.isBefore(today)) {
-//   rowEl.addClass('project-late');
-// } else if (projectDate.isSame(today)) {
-//   rowEl.addClass('project-today');
-// }
-
-compareToCurrentTime ();
-displayDateAndTime();
-setInterval(displayDateAndTime, 1000);
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
+        // update the before and new now
+        before = rightNow;
+        rightNow = rightNow.add(1, 's');
+        
+    }, 1000);
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
 
-  // TODO: Add code to display the current date in the header of the page.
+    // adds click listener to save button
+    // for text -indicates "this" is a sibling of the save button called "plans" which is the class in the text input area
+    // for time - indicates "this" is the parent of the save button with an attribute of "id" which stores the time value as part of the "id"
+    // text stores the user input plans, time stores the "id" of each time block div which contains the hour of each time block
+    $('.saveBtn').click(function () {
+        const newText = $(this).siblings('.plans').val();
+        const hour = $(this).parent().attr('id').split('hour')[1];
+        //stores the time and text objects in local storage
+        hourTexts[hour] = newText;
+        localStorage.setItem("hourTexts", JSON.stringify(hourTexts));
+    });
+
+    //retrieves plans input from local storage which also contains time value we want to parse and split
+    
+    // load saved hour texts from before
+    const savedHourTexts = localStorage.getItem("hourTexts"); 
+    if (savedHourTexts !== null){
+        hourTexts = JSON.parse(savedHourTexts);
+    }
+    const currentHour = rightNow.hour();// dayjs().hour();
+    // for key in object
+    for (const hour in hourTexts) {
+        const text = hourTexts[hour];
+        const timeBlockTextEl = $(`#hour${hour} .plans`).eq(0);
+        timeBlockTextEl.val(text);
+        // update time block styles/class
+        timeBlockTextEl.removeClass('future');
+        timeBlockTextEl.removeClass('present');
+        timeBlockTextEl.removeClass('past');
+
+        const hourInteger = parseInt(hour);
+        if (hourInteger < currentHour) {
+            timeBlockTextEl.addClass('past');
+        }
+        else if (hourInteger === currentHour) {
+            timeBlockTextEl.addClass('present');
+        }
+        else {
+            timeBlockTextEl.addClass('future');
+        }
+    }
+
+
+
+
+    // $('#hour9 .plans').val(localStorage.getItem("hour9"));
+    // $('#hour10 .plans').val(localStorage.getItem("hour10"));
+    // $('#hour12 .plans').val(localStorage.getItem("hour11"));
+    // $('#hour13 .plans').val(localStorage.getItem("hour12"));
+    // $('#hour14 .plans').val(localStorage.getItem("hour13"));
+    // $('#hour15 .plans').val(localStorage.getItem("hour14"));
+    // $('#hour15 .plans').val(localStorage.getItem("hour15"));
+    // $('#hour16 .plans').val(localStorage.getItem("hour16"));
+    // $('#hour17 .plans').val(localStorage.getItem("hour17"));
+
+    // function compareTime() {
+    //     let currentHour = dayjs().hour();
+
+    //     $('.time-block').each(function () {
+    //         let timeCode = parseInt($(this).attr('id').split('hour')[1]);
+
+    //         if (timeCode < currentHour) {
+    //             $(this).removeClass('future');
+    //             $(this).removeClass('present');
+    //             $(this).addClass('past');
+    //         }
+    //         else if (timeCode === currentHour) {
+    //             $(this).removeClass('past');
+    //             $(this).removeClass('future');
+    //             $(this).addClass('present');
+    //         }
+    //         else {
+    //             $(this).removeClass('past');
+    //             $(this).removeClass('present');
+    //             $(this).addClass('future');
+    //         }
+    //     })
+    // }
 });
+
